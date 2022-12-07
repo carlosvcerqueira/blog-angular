1212b98c73586dc1066c737b2def7d711270de3f
carlosvcerqueira <carlosvcerqueira@gmail.com>
2022-12-07 15:55:11 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"saiu novo filme",
+    "description":"ola",
+    "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+  },
+  {
+    "id":"2",
+    "title":"saiu outro filme",
+    "description":"oi",
+    "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+  }
+]
